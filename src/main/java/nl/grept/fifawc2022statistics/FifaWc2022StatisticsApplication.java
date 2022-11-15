b5573879270a446b60b601d727a9b3f2fd5d761d
Grept <tomjansen.408@gmail.com>
2022-11-15 15:00:44 +0100

    First Commit.

---
+++ src/main/java/nl/grept/fifawc2022statistics/FifaWc2022StatisticsApplication.java
@@ -0,0 +1,13 @@
+package nl.grept.fifawc2022statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaWc2022StatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifaWc2022StatisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/grept/fifawc2022statistics/FifaWc2022StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.grept.fifawc2022statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaWc2022StatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
