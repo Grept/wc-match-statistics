328403eb60c4489cb6e70d69caaa4425acc5e3ab
Grept <tomjansen.408@gmail.com>
2022-11-30 12:15:19 +0100

    BE: added some fields to the Team class.

--- src/main/java/nl/grept/fifawc2022statistics/model/Team.java
+++ src/main/java/nl/grept/fifawc2022statistics/model/Team.java
@@ -1,19 +1,26 @@
 package nl.grept.fifawc2022statistics.model;
 
-import org.springframework.data.annotation.Id;
+import lombok.Data;
 import org.springframework.data.annotation.Transient;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Data
 public class Team {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String countryName;
+    private String teamName;
+    private Integer fifaRank;
+    private Integer goalDifference;
 
     @Transient
-    private List<Match> matches;
+    private List<Match> matches = new ArrayList<>();
 }
